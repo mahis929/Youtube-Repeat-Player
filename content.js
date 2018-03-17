b19011af0ca0708530652e9a6e40536c4ed2b423
kryptokinght <minanshu.1998@gmail.com>
2018-03-17 14:03:01 +0530

    browserAction on click load content script

--- content.js
+++ content.js
@@ -1,32 +1,15 @@
-console.log("Smart pause content.js loaded");
+console.log("YTR content.js has loaded");
 
-var reg1 = /soundcloud/g;
-var reg2 = /youtube/g;
-var reg3 = /asd/g;
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if(request.value === 'content') {			
-		console.log("Message received from background.js!!");
-		console.log(request.url);
-		if(reg1.test(request.url)) {
-			let btn = document.getElementsByClassName('playControl');
-			if(!request.active)
-				btn[0].click();
-			else if(request.active)
-				btn[0].click();
-		}
-		else if(reg2.test(request.url)) {
-			let vid = document.getElementsByTagName('video');
-			if(!request.active)
-				vid[0].pause();
-			else
-				vid[0].play();
-		}	
-		else if(reg3.test(request.url)) {
-			let vid = document.getElementsByTagName('video');
-			if(!request.active)
-				vid[0].pause();
-			else
-				vid[0].play();
-		}
-	}
-});
+//listens for message from background.js
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse) {
+	var body = document.getElementsByTagName('body');
+	
+	var div = document.createElement('DIV');
+	div.setAttribute("id", "yrp-player");
+	div.style.zIndex = "1000";
+	div.innerHTML = "<h1 style = "color:red;">HJADJHASGDJ</h1>";
+
+	body[0].appendChild(div);
+}
--- popup.js
+++ popup.js
@@ -1,6 +1,21 @@
-console.log("Smart pause popup.js loaded!!");
-
-chrome.runtime.sendMessage({value: "bg"}, function(response){
-	console.log("Message sent!!");
-});
-
+console.log("YTR popup.js loaded!!");
+document.getElementById('submit').addEventListener('click', clicked);
+ 
+function clicked() {
+	let qty = document.getElementById('qty').value;
+	//from popup to content.js
+	var params = {
+	  active : true,
+	  currentWindow : true
+	}
+	chrome.tabs.query(params, gotTabs);
+	function gotTabs(tabs) {
+	  	let msg = {
+	      value : true,
+	      qty: qty
+	    };    
+		chrome.tabs.sendMessage(tabs[0].id, msg, function(response) {
+			console.log("Message sent!");
+		});
+	}	
+}
