93fd868300fe1060a24923b7cbf05957edd413fe
kryptokinght <minanshu.1998@gmail.com>
2018-03-17 14:05:36 +0530

    update content.js comment added to gotMessage

--- content.js
+++ content.js
@@ -4,12 +4,13 @@ console.log("YTR content.js has loaded");
 chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
+	console.log("BrowserAction clicked, Music player loaded!!");
 	var body = document.getElementsByTagName('body');
 	
 	var div = document.createElement('DIV');
 	div.setAttribute("id", "yrp-player");
 	div.style.zIndex = "1000";
-	div.innerHTML = "<h1 style = "color:red;">HJADJHASGDJ</h1>";
+	div.innerHTML = "<h1 style = 'color:red;'>HJADJHASGDJ</h1>";
 
 	body[0].appendChild(div);
 }
